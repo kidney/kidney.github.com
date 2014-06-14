101e3f5fb498221db7ee54734579e008e60ee86e
kidney <kidneyleung@gmail.com>
2014-06-14 18:46:33 +0800

    update

--- assets/js/page.js
+++ assets/js/page.js
@@ -18,6 +18,16 @@ define('page', ['$'], function(require, exports, module) {
                 scrollTop: 0
             }, 800);
         });
+
+        $('#page').on('click', '[data-role]', function(event) {
+            event.preventDefault();
+
+            var $target = $(event.currentTarget);
+            if ($target.attr('data-role') == 'navbar-toggle') {
+                var $nav = $('#page').find('[data-role=navbar-collapse]');
+                $nav.length && $nav.toggleClass('show');
+            }
+        });
     }
 
     $(document).ready(pageInit);
